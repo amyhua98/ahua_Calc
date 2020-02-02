6f362b24349c18f8c11498e71f357322b65de4b0
Amy Hua <aymay98@gmail.com>
2020-02-01 20:18:01 -0600

    Initial Commit

---
+++ ahua_Calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ahua_Calc
+//
+//  Created by Amy Hua on 2/1/20.
+//  Copyright © 2020 DePaul University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
