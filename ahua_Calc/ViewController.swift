627d5a663d9c78c3519bddccd5862edc3740f7b2
Amy Hua <aymay98@gmail.com>
2020-09-07 14:17:16 -0500

    First commit to new Calc

--- ahua_Calc/ViewController.swift
+++ ahua_Calc/ViewController.swift
@@ -9,7 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var initalNum: Int = 0
+    var prevNum: Int = 0
+    var currNum = false
+    var equals = 0
 
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func numbers(_ sender: UIButton) {
+        if currNum == true{
+            display.text = String(sender.tag-1)
+            initalNum = Int(display.text!)!
+            currNum = false
+        }
+        else{
+            display.text = display.text! + String(sender.tag-1)
+            initalNum = Int(display.text!)!
+        }
+    }
+    
+    
+    @IBAction func signs(_ sender: UIButton) {
+        if display.text != "" && sender.tag != 11 && sender.tag != 14{
+            
+            prevNum = Int(display.text!)!
+            if sender.tag == 12{
+                display.text = "+"
+            }
+            else if sender.tag == 13{
+                display.text = "-"
+            }
+            equals = sender.tag
+            currNum = true
+        }
+        else if sender.tag == 11{
+            if equals == 12{
+                display.text = String(prevNum + initalNum)
+            }
+            else if equals == 13{
+                display.text = String(prevNum - initalNum)
+            }
+        }
+        else if sender.tag == 14{
+            display.text = ""
+            prevNum = 0
+            currNum = true
+            equals = 0
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,4 +66,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
